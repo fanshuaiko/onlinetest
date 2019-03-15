c04cfb7b81ba41f02065a35e098d218fca2746a7
fanshuaiko <1197303677@qq.com>
2019-03-15 16:06:56 +0800

    配置注册中心地址，用于发现本服务

--- front/src/main/java/com/fanshuaiko/front/FrontApplication.java
+++ front/src/main/java/com/fanshuaiko/front/FrontApplication.java
@@ -2,8 +2,10 @@ package com.fanshuaiko.front;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FrontApplication {
 
     public static void main(String[] args) {
