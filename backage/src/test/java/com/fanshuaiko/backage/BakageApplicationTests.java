8488f6eb66bec29656f2163f790250e72c027c06
fanshuaiko <1197303677@qq.com>
2019-03-21 12:19:03 +0800

    MD5加密密码

--- backage/src/test/java/com/fanshuaiko/backage/BakageApplicationTests.java
+++ backage/src/test/java/com/fanshuaiko/backage/BakageApplicationTests.java
@@ -1,5 +1,7 @@
 package com.fanshuaiko.backage;
 
+import org.apache.shiro.crypto.hash.SimpleHash;
+import org.apache.shiro.util.ByteSource;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,7 +12,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class BakageApplicationTests {
 
     @Test
-    public void contextLoads() {
+    public void createMD5() {
+        String hashAlgorithmName = "md5";
+        String credentials = "111111";//密码
+        int hashIterations = 2;
+        ByteSource credentialsSalt = ByteSource.Util.bytes("151616");//账号
+        System.out.println("credentialsSalt"+credentialsSalt);
+        String  obj = new SimpleHash(hashAlgorithmName, credentials,credentialsSalt, hashIterations).toHex();
+        System.out.println(obj);
     }
 
 }
