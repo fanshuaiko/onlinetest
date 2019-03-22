e9001e6c7851315b5a7888e3c4d5e59f80fbe5a5
fanshuaiko <1197303677@qq.com>
2019-03-22 14:12:22 +0800

    加上mybatis扫描mapper注解

--- backage/src/main/java/com/fanshuaiko/backage/BackageApplication.java
+++ backage/src/main/java/com/fanshuaiko/backage/BackageApplication.java
@@ -1,11 +1,13 @@
 package com.fanshuaiko.backage;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@MapperScan("com.fanshuaiko.backage.dao")
 public class BackageApplication {
 
     public static void main(String[] args) {
