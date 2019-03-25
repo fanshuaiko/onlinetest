d55cc2d9e00b86a5d2d198a71edcaa444c64f267
fanshuaiko <1197303677@qq.com>
2019-03-25 16:33:22 +0800

    批量添加和多条件查询

---
+++ backage/src/main/java/com/fanshuaiko/backage/dao/SubjectiveDao.java
@@ -0,0 +1,25 @@
+package com.fanshuaiko.backage.dao;
+
+import com.fanshuaiko.backage.entity.QuestionQueryTerm;
+import com.fanshuaiko.backage.entity.Subjective;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface SubjectiveDao {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Subjective record);
+
+    int insertSelective(Subjective record);
+
+    Subjective selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(Subjective record);
+
+    int updateByPrimaryKey(Subjective record);
+
+    List<Subjective> queryByConditions(QuestionQueryTerm queryTerm); //多条件查询
+
+    int batchAdd(@Param("subjectiveList") List<Subjective> subjectiveList);//批量添加题目
+}
