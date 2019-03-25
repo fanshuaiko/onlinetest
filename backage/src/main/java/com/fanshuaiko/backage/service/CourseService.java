b97d09c3e047b07a0c5905232af25c55dc8c8e59
fanshuaiko <1197303677@qq.com>
2019-03-25 15:59:35 +0800

    课程service

---
+++ backage/src/main/java/com/fanshuaiko/backage/service/CourseService.java
@@ -0,0 +1,11 @@
+package com.fanshuaiko.backage.service;
+
+/**
+ * @ClassName CourseService
+ * @Description
+ * @Author fanshuaiko
+ * @Date 19-3-25 上午10:28
+ * @Version 1.0
+ **/
+public interface CourseService {
+}
