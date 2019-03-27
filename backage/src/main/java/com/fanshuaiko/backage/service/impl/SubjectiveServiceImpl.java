2466cb6bfb2ef386c384802474bcc4a43de1facf
fanshuaiko <1197303677@qq.com>
2019-03-27 16:19:28 +0800

    将上传文件解析单独放进ImportUtil类中

--- backage/src/main/java/com/fanshuaiko/backage/service/impl/SubjectiveServiceImpl.java
+++ backage/src/main/java/com/fanshuaiko/backage/service/impl/SubjectiveServiceImpl.java
@@ -10,6 +10,7 @@ import com.fanshuaiko.backage.entity.QuestionQueryTerm;
 import com.fanshuaiko.backage.entity.Subjective;
 import com.fanshuaiko.backage.service.SubjectiveService;
 import com.fanshuaiko.backage.utils.ErrorCode;
+import com.fanshuaiko.backage.utils.ImportUtil;
 import com.fanshuaiko.backage.utils.ResultData;
 import com.fanshuaiko.backage.utils.SnowflakeIdWorker;
 import com.github.pagehelper.PageHelper;
@@ -67,7 +68,7 @@ public class SubjectiveServiceImpl implements SubjectiveService {
 
     public ResultData importChoice(MultipartFile file, String type){
         try {
-            ResultData resultData = checkImportSubjective(file, type);
+            ResultData resultData = ImportUtil.checkImportSubjective(file, type);
             if (resultData.getData() == null) {
                 return resultData;
             }
@@ -79,43 +80,5 @@ public class SubjectiveServiceImpl implements SubjectiveService {
         }
     }
 
-    public ResultData checkImportSubjective(MultipartFile file, String type) {
-        ImportParams importParams = new ImportParams();
-        importParams.setTitleRows(1);
-        importParams.setHeadRows(1);
-        try {
-            List<Subjective> subjectiveList = ExcelImportUtil.importExcel(file.getInputStream(), Subjective.class, importParams);
-            //去除空元素
-            subjectiveList.removeAll(Collections.singleton(null));
-            //判断上传文件是否为空
-            if (subjectiveList.isEmpty()) {
-                return ResultData.newResultData(ErrorCode.FAILOR, "导入数据失败，数据不能为空，请检查后重新导入");
-            }
-            for (Subjective subjective : subjectiveList) {
-                ResultData<String> data = checkNull(subjective, type);
-                if (data != null) {
-                    return data;
-                }
-                subjective.setId(SnowflakeIdWorker.nextId());
-                subjective.setType(type);
-            }
-            return ResultData.newSuccessResultData(subjectiveList);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ResultData.newResultData(ErrorCode.ADD_FAILOR_MSG, "上传失败！");
-        }
 
-    }
-
-    //判断上传的选择题表格的每一列是否为空
-    public static ResultData<String> checkNull(Subjective subjective, String type) {
-        if (StringUtils.isEmpty(subjective.getQuestion())) {
-            return ResultData.newResultData(ErrorCode.FAILOR, "题目不能为空！");
-        } else if (StringUtils.isEmpty(subjective.getAnswer())) {
-            return ResultData.newResultData(ErrorCode.FAILOR, "题目：\'" + subjective.getQuestion() + "\'的答案不能为空！");
-        } else if (StringUtils.isEmpty(subjective.getCourseName())) {
-            return ResultData.newResultData(ErrorCode.FAILOR, "题目：\'" + subjective.getQuestion() + "\'的所属课程不能为空！");
-        }
-        return null;
-    }
 }
