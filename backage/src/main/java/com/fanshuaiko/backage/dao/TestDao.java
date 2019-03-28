3ecf25a9a01488cebc8c86e064745fad6561404e
fanshuaiko <1197303677@qq.com>
2019-03-28 11:04:39 +0800

    保存考试的相关信息

--- backage/src/main/java/com/fanshuaiko/backage/dao/TestDao.java
+++ backage/src/main/java/com/fanshuaiko/backage/dao/TestDao.java
@@ -3,7 +3,10 @@ package com.fanshuaiko.backage.dao;
 import com.fanshuaiko.backage.entity.Test;
 import com.fanshuaiko.backage.entity.VO.TestVO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Mapper
 @Repository
@@ -20,6 +23,10 @@ public interface TestDao {
 
     int updateByPrimaryKey(Test record);
 
-    int insertTest(TestVO testVO);
+    int insertTest(TestVO testVO); //保存考试信息到test表
 
+    int insertTestClass(TestVO testVO); //保存参与考试的班级到test_class表
+
+    int insertTestQuestion(@Param("testNo") long testNo, @Param("questionNoList") List<Long> questionNoList,
+                           @Param("score") int score, @Param("questionType") String questionType); //保存考试题目信息到test_question 表
 }
