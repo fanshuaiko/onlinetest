645fe6d7783db3eac1dfb1bb86336874907e9b52
fanshuaiko <1197303677@qq.com>
2019-04-01 15:06:19 +0800

    增加是否批改字段

--- backage/src/main/java/com/fanshuaiko/backage/dao/ScoreDetailDao.java
+++ backage/src/main/java/com/fanshuaiko/backage/dao/ScoreDetailDao.java
@@ -1,7 +1,11 @@
 package com.fanshuaiko.backage.dao;
 
 import com.fanshuaiko.backage.entity.ScoreDetail;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
+@Mapper
+@Repository
 public interface ScoreDetailDao {
     int insert(ScoreDetail record);
 
