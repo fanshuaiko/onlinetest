14b4746812238fd3d22c56815f318e365e279304
fanshuaiko <1197303677@qq.com>
2019-04-01 14:41:33 +0800

    删除考试

---
+++ backage/src/main/java/com/fanshuaiko/backage/entity/ScoreDetail.java
@@ -0,0 +1,91 @@
+package com.fanshuaiko.backage.entity;
+
+import java.util.Date;
+/**
+ * @ClassName ScoreDetail
+ * @Description 得分详细实体类
+ * @Author fanshuaiko
+ * @Date 19-3-15 上午10:40
+ * @Version 1.0
+ **/
+public class ScoreDetail {
+    private Long id;
+
+    private String studentNo;
+
+    private Long testNo;
+
+    private Long questionNo;
+
+    private String studentScore;
+
+    private String questionType;
+
+    private Date createTime;
+
+    private String studentAnswer;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStudentNo() {
+        return studentNo;
+    }
+
+    public void setStudentNo(String studentNo) {
+        this.studentNo = studentNo == null ? null : studentNo.trim();
+    }
+
+    public Long getTestNo() {
+        return testNo;
+    }
+
+    public void setTestNo(Long testNo) {
+        this.testNo = testNo;
+    }
+
+    public Long getQuestionNo() {
+        return questionNo;
+    }
+
+    public void setQuestionNo(Long questionNo) {
+        this.questionNo = questionNo;
+    }
+
+    public String getStudentScore() {
+        return studentScore;
+    }
+
+    public void setStudentScore(String studentScore) {
+        this.studentScore = studentScore == null ? null : studentScore.trim();
+    }
+
+    public String getQuestionType() {
+        return questionType;
+    }
+
+    public void setQuestionType(String questionType) {
+        this.questionType = questionType == null ? null : questionType.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getStudentAnswer() {
+        return studentAnswer;
+    }
+
+    public void setStudentAnswer(String studentAnswer) {
+        this.studentAnswer = studentAnswer == null ? null : studentAnswer.trim();
+    }
+}
