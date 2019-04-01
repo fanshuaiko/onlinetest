dbcaec8daa2ae3784848f20d6ef3a68ec8f84e42
fanshuaiko <1197303677@qq.com>
2019-04-01 17:28:32 +0800

    将登录模块移除

--- backage/src/test/java/com/fanshuaiko/backage/BakageApplicationTests.java
+++ backage/src/test/java/com/fanshuaiko/backage/BakageApplicationTests.java
@@ -5,7 +5,11 @@ import org.apache.shiro.util.ByteSource;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+import java.util.LinkedList;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -16,10 +20,16 @@ public class BakageApplicationTests {
         String hashAlgorithmName = "md5";
         String credentials = "111111";//密码
         int hashIterations = 2;
-        ByteSource credentialsSalt = ByteSource.Util.bytes("151616");//账号
+        ByteSource credentialsSalt = ByteSource.Util.bytes("t10001");//账号
         System.out.println("credentialsSalt"+credentialsSalt);
         String  obj = new SimpleHash(hashAlgorithmName, credentials,credentialsSalt, hashIterations).toHex();
-        System.out.println(obj);
+        System.out.println("========"+obj);
     }
+
+
+    @Test
+    public static void main(String[] args){
+    }
+
 
 }
