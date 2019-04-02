1d0319c63feb1faf74a298d57a58eb0e87a20930
fanshuaiko <1197303677@qq.com>
2019-04-02 16:58:05 +0800

    批量改改学生选择题得分

--- backage/src/main/java/com/fanshuaiko/backage/dao/ScoreDetailDao.java
+++ backage/src/main/java/com/fanshuaiko/backage/dao/ScoreDetailDao.java
@@ -6,6 +6,8 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Mapper
 @Repository
 public interface ScoreDetailDao {
@@ -16,4 +18,8 @@ public interface ScoreDetailDao {
     ScoreDetailReturnVo queryScoreDetail(long testNo); // 根据考试编码查询未批改的主观题
 
     int updateStudentScoreById(@Param("id") long id, @Param("score") int score); //根据score_detail的id更新学生题目得分
+
+    List<ScoreDetail> queryAllChoiceByTestNo(Long testNo); //根据考试编号查询所有选择题的得分信息
+
+    int batchUpdateStudentScore(@Param("scoreDetails") List<ScoreDetail> scoreDetails); //批量更新学生得分
 }
