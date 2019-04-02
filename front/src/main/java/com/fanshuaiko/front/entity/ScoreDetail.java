d8b5954b3d886e4adae0166a9b1b4f9cda7f66df
fanshuaiko <1197303677@qq.com>
2019-04-02 11:09:30 +0800

    题目得分

---
+++ front/src/main/java/com/fanshuaiko/front/entity/ScoreDetail.java
@@ -0,0 +1,101 @@
+package com.fanshuaiko.front.entity;
+
+import java.util.Date;
+/**
+ * @ClassName ScoreDetail
+ * @Description 题目得分
+ * @Author fanshuaiko
+ * @Date 19-4-2 上午10:00
+ * @Version 1.0
+ **/
+public class ScoreDetail {
+    private Integer id;
+
+    private String studentNo;
+
+    private Long testNo;
+
+    private Long questionNo;
+
+    private String studentScore;
+
+    private String questionType;
+
+    private String flag; //0.未批改，1.已批改
+
+    private String createTime;
+
+    private String studentAnswer;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getStudentNo() {
+        return studentNo;
+    }
+
+    public void setStudentNo(String studentNo) {
+        this.studentNo = studentNo == null ? null : studentNo.trim();
+    }
+
+    public Long getTestNo() {
+        return testNo;
+    }
+
+    public void setTestNo(Long testNo) {
+        this.testNo = testNo;
+    }
+
+    public Long getQuestionNo() {
+        return questionNo;
+    }
+
+    public void setQuestionNo(Long questionNo) {
+        this.questionNo = questionNo;
+    }
+
+    public String getStudentScore() {
+        return studentScore;
+    }
+
+    public void setStudentScore(String studentScore) {
+        this.studentScore = studentScore == null ? null : studentScore.trim();
+    }
+
+    public String getQuestionType() {
+        return questionType;
+    }
+
+    public void setQuestionType(String questionType) {
+        this.questionType = questionType == null ? null : questionType.trim();
+    }
+
+    public String getFlag() {
+        return flag;
+    }
+
+    public void setFlag(String flag) {
+        this.flag = flag == null ? null : flag.trim();
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getStudentAnswer() {
+        return studentAnswer;
+    }
+
+    public void setStudentAnswer(String studentAnswer) {
+        this.studentAnswer = studentAnswer == null ? null : studentAnswer.trim();
+    }
+}
