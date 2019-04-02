7619fc5fc82e445fccfc30aadbc8cf3bf4cb793c
fanshuaiko <1197303677@qq.com>
2019-04-02 10:46:26 +0800

    查询所有题目时使用redis缓存

--- front/src/main/java/com/fanshuaiko/front/service/impl/TestServiceImpl.java
+++ front/src/main/java/com/fanshuaiko/front/service/impl/TestServiceImpl.java
@@ -6,10 +6,14 @@ import com.fanshuaiko.front.entity.Test;
 import com.fanshuaiko.front.service.TestService;
 import com.fanshuaiko.front.utils.ResultData;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
+import java.awt.*;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @ClassName TestServiceImpl
@@ -23,6 +27,8 @@ public class TestServiceImpl implements TestService {
 
     @Autowired
     private TestDao testDao;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @Override
     public ResultData queryTest(String studentNo) {
@@ -32,11 +38,16 @@ public class TestServiceImpl implements TestService {
 
     @Override
     public ResultData getQuestion(long testNo) {
-        List<Question> choiceList = testDao.getChoice(testNo);
-        List<Question> subjectiveList = testDao.getSubjective(testNo);
-        LinkedList<Question> questions = new LinkedList<>();
-        questions.addAll(choiceList);
-        questions.addAll(subjectiveList);
-        return ResultData.newSuccessResultData(questions);
+        List<Question> questionList = (List<Question>) redisTemplate.opsForValue().get(testNo);
+        if(CollectionUtils.isEmpty(questionList)){
+            List<Question> choiceList = testDao.getChoice(testNo);
+            List<Question> subjectiveList = testDao.getSubjective(testNo);
+            questionList.addAll(choiceList);
+            questionList.addAll(subjectiveList);
+            redisTemplate.opsForValue().set(testNo,questionList);
+            redisTemplate.expire(testNo,1, TimeUnit.HOURS);
+
+        }
+        return ResultData.newSuccessResultData(questionList);
     }
 }
