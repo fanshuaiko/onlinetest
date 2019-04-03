e7fa1a26909414d569771270123cd8b643723ab2
fanshuaiko <1197303677@qq.com>
2019-04-03 15:04:55 +0800

    文件下载工具类

---
+++ backage/src/main/java/com/fanshuaiko/backage/utils/ExportUtil.java
@@ -0,0 +1,33 @@
+package com.fanshuaiko.backage.utils;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @ClassName ExportUtil
+ * @Description 文件下载工具类
+ * @Author fanshuaiko
+ * @Date 19-4-3 下午2:48
+ * @Version 1.0
+ **/
+public class ExportUtil {
+    public static void exportExcel(HttpServletResponse response, InputStream in,String name){
+        try {
+            byte[] buffer = new byte[in.available()];
+            in.read(buffer);
+            in.close();
+            String fileName = new String(name.getBytes("UTF-8"));
+            response.setCharacterEncoding("UTF-8");
+            response.setHeader("Content-disposition","attachment;filename="+fileName);
+            BufferedOutputStream outputStream = new BufferedOutputStream(response.getOutputStream());
+            response.setContentType("application/octet-stream");
+            outputStream.write(buffer);
+            outputStream.flush();
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
