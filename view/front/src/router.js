3bbd94b8f177e3deaaf0f30c346f3d2d64bd1641
fanshuaiko <1197303677@qq.com>
2019-04-08 09:40:41 +0800

    增加login路由

--- view/front/src/router.js
+++ view/front/src/router.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+// import Home from './views/Home.vue'
+import ShowTests from './views/ShowTests.vue'
+import Login from "@/views/Login";
+
 
 Vue.use(Router)
 
@@ -8,18 +11,16 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
+
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'login',
+      component: Login
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/tests',
+      name: 'tests',
+      component: ShowTests
     }
   ]
 })
