ece37ee7d14343e2f8c50ff540c01a93d1455eaa
fanshuaiko <1197303677@qq.com>
2019-04-08 09:53:18 +0800

    展示考试页面

---
+++ view/front/src/views/Tests.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <div class="content">
+            <table class="am-table am-table-striped am-table-hover">
+                <tr>
+                    <th>考试id</th>
+                    <th>考试</th>
+                    <th>所属课程</th>
+                    <th>任课教师</th>
+                    <th>总分</th>
+                    <th>考试状态</th>
+                    <th>开考时间</th>
+                    <th>考试时长</th>
+                </tr>
+                <tr>
+                    <td>rewtre</td>
+                    <td>ewrtretae</td>
+                    <td>etrrr</td>
+                    <td>eyryr</td>
+                    <td>eyry</td>
+                    <td>ryyy</td>
+                    <td>yyy</td>
+                    <td>uuuu</td>
+                </tr>
+                <tr>
+                    <td>rewtre</td>
+                    <td>ewrtretae</td>
+                    <td>etrrr</td>
+                    <td>eyryr</td>
+                    <td>eyry</td>
+                    <td>ryyy</td>
+                    <td>yyy</td>
+                    <td>uuuu</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ShowTests"
+    }
+</script>
+
+<style scoped>
+    /*@import "http://cdn.amazeui.org/amazeui/2.7.2/css/amazeui.min.css";*/
+
+    /*.content {*/
+    /*    background: seagreen;*/
+    /*    position: absolute;*/
+    /*    width: 60%;*/
+    /*    height: 40%;*/
+    /*    left: 50%;*/
+    /*    top: 50%;*/
+    /*    transform: translate(-50%, -50%);*/
+    /*    border-radius: 20px;*/
+    /*    box-shadow: #42b983 0px 0px 30px 5px;*/
+    /*}*/
+</style>
