bbef6b9afd339f7fd603a5d0193dc0c4c89d52b5
fanshuaiko <1197303677@qq.com>
2019-04-08 19:08:12 +0800

    initial

--- view/front/src/App.vue
+++ view/front/src/App.vue
@@ -1,16 +1,16 @@
 <template>
-    <div class="app">
-        <div>
-            <router-link to="/"></router-link>
-        </div>
-            <router-view></router-view>
+  <div class="app">
+    <div>
+      <router-link to="/"></router-link>
     </div>
+    <router-view></router-view>
+  </div>
 </template>
-
-<style>
-
-</style>
 
 <script>
 
 </script>
+
+<style>
+
+</style>
--- view/front/src/router/index.js
+++ view/front/src/router/index.js
@@ -1,17 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import Home from './views/Home.vue'
-import ShowTests from './views/Tests.vue'
-import Login from "@/views/Login";
-
+import Login from "../views/Login";
+import Tests from "../views/Tests";
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
   routes: [
-
     {
       path: '/',
       name: 'login',
@@ -20,7 +15,7 @@ export default new Router({
     {
       path: '/tests',
       name: 'tests',
-      component: ShowTests
+      component: Tests
     }
   ]
 })
