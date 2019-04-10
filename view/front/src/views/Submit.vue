eea1f24c4241053fef906dd1d03fa7fec0f348f6
fanshuaiko <1197303677@qq.com>
2019-04-10 21:19:55 +0800

    加一句提示

--- view/front/src/views/Submit.vue
+++ view/front/src/views/Submit.vue
@@ -1,13 +1,19 @@
 <template>
-
+  <div>
+    <div class="main">
+      <h1>您已完成考试，关闭页面即可！</h1>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Submit"
-    }
+  export default {
+    name: "Submit"
+  }
 </script>
 
-<style scoped>
-
+<style>
+  .main {
+  text-align: center;
+  }
 </style>
