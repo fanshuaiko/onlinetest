daee624ca7188244b750c07a7a9dcf573847f416
fanshuaiko <1197303677@qq.com>
2019-04-11 15:32:12 +0800

    添加main路由

--- view/backage/src/router/index.js
+++ view/backage/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from "../views/Login";
+import Main from "../views/Main";
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'login',
       component: Login
+    },
+    {
+      path: '/main',
+      name: 'main',
+      component: Main
     }
   ]
 })
