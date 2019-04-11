599cfa88f40ec310f5c325d0152ec41553d5cfc7
fanshuaiko <1197303677@qq.com>
2019-04-11 17:27:32 +0800

    解决long类型数据传到前台丢失精度

---
+++ backage/src/main/java/com/fanshuaiko/backage/config/JsonLongToString.java
@@ -0,0 +1,41 @@
+package com.fanshuaiko.backage.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
+
+/**
+ * @ClassName JsonLongToString
+ * @Description
+ * @Author fanshuaiko
+ * @Date 19-4-9 下午4:58
+ * @Version 1.0
+ **/
+
+@Configuration
+@EnableWebMvc
+public class JsonLongToString extends WebMvcConfigurerAdapter {
+
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        MappingJackson2HttpMessageConverter jackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper objectMapper = new ObjectMapper();
+        /**
+         * 序列换成json时,将所有的long变成string
+         * 因为js中得数字类型不能包含所有的java long值
+         */
+        SimpleModule simpleModule = new SimpleModule();
+        simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
+        simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
+        objectMapper.registerModule(simpleModule);
+        jackson2HttpMessageConverter.setObjectMapper(objectMapper);
+        converters.add(jackson2HttpMessageConverter);
+    }
+}
+
