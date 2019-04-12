c46186c4025bf29e18f7aee156e423dc4aa90d9a
fanshuaiko <1197303677@qq.com>
2019-04-12 18:03:35 +0800

    全局api管理

---
+++ view/backage/src/api/api.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+let backage_base = 'backage-api/test';
+
+//根据id删除考试
+export const deleteTestByIds = params => {
+  return axios.delete(`${backage_base}/test/` + params)
+};
+
+//根据教师编号分页查询考试
+export const pageQueryTest = params => {
+  return axios.get(`${backage_base}/test/` + params.pageNum + '/' + params.pageSize + '/' + params.username);
+};
+
+//查询所有课程
+export const getAllCourse = params =>{
+  return axios.get(`${backage_base}/course/course`)
+}
+
