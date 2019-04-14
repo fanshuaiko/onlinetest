c0f7791b5cfe7c92979aa09e76391e0f7b365c44
fanshuaiko <1197303677@qq.com>
2019-04-14 20:13:01 +0800

    修改导航栏

--- view/backage/src/views/Main.vue
+++ view/backage/src/views/Main.vue
@@ -11,30 +11,23 @@
           <template slot="title"><i class="el-icon-message"></i>考试</template>
           <el-menu-item-group>
             <el-menu-item index="1-1">所有考试</el-menu-item>
-            <el-menu-item index="1-2">选项2</el-menu-item>
           </el-menu-item-group>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="1-3">选项3</el-menu-item>
-          </el-menu-item-group>
-          <el-submenu index="1-4">
-            <template slot="title">选项4</template>
-            <el-menu-item index="1-4-1">选项4-1</el-menu-item>
-          </el-submenu>
         </el-submenu>
         <el-submenu index="2">
           <template slot="title"><i class="el-icon-menu"></i>题目管理</template>
-          <el-menu-item-group>
-            <template slot="title">分组一</template>
-            <el-menu-item index="2-1">选项1</el-menu-item>
-            <el-menu-item index="2-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="2-3">选项3</el-menu-item>
-          </el-menu-item-group>
+            <el-menu-item index="2-3">选择题管理</el-menu-item>
+            <el-menu-item index="2-3">主观题管理</el-menu-item>
           <el-submenu index="2-4">
             <template slot="title">选项4</template>
             <el-menu-item index="2-4-1">选项4-1</el-menu-item>
           </el-submenu>
+        </el-submenu>
+        <el-submenu index="1">
+          <template slot="title"><i class="el-icon-message"></i>阅卷</template>
+          <el-menu-item-group>
+            <el-menu-item index="1-1">选项</el-menu-item>
+            <el-menu-item index="1-1">选项</el-menu-item>
+          </el-menu-item-group>
         </el-submenu>
         <el-submenu index="3">
           <template slot="title"><i class="el-icon-setting"></i>考试分析</template>
