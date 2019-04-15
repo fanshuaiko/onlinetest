aac6bd9ea2801375ef3201fab5445a971521cad2
fanshuaiko <1197303677@qq.com>
2019-04-15 14:27:08 +0800

    添加选择题管理和主观题管理的页面路由

--- view/backage/src/views/Main.vue
+++ view/backage/src/views/Main.vue
@@ -16,34 +16,30 @@
         <el-submenu index="2">
           <template slot="title"><i class="el-icon-menu"></i>题目管理</template>
           <el-menu-item-group>
-            <el-menu-item index="2-3">选择题管理</el-menu-item>
-            <el-menu-item index="2-3">主观题管理</el-menu-item>
-            <el-submenu index="2-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="2-4-1">选项4-1</el-menu-item>
-            </el-submenu>
-          </el-menu-item-group>
-        </el-submenu>
-        <el-submenu index="1">
-          <template slot="title"><i class="el-icon-message"></i>阅卷</template>
-          <el-menu-item-group>
-            <el-menu-item index="1-1">选项</el-menu-item>
-            <el-menu-item index="1-1">选项</el-menu-item>
+            <el-menu-item index="2-1">选择题管理</el-menu-item>
+            <el-menu-item index="2-2">主观题管理</el-menu-item>
           </el-menu-item-group>
         </el-submenu>
         <el-submenu index="3">
+          <template slot="title"><i class="el-icon-message"></i>阅卷</template>
+          <el-menu-item-group>
+            <el-menu-item index="3-1">选项</el-menu-item>
+            <el-menu-item index="3-1">选项</el-menu-item>
+          </el-menu-item-group>
+        </el-submenu>
+        <el-submenu index="4">
           <template slot="title"><i class="el-icon-setting"></i>考试分析</template>
           <el-menu-item-group>
             <template slot="title">分组一</template>
-            <el-menu-item index="3-1">选项1</el-menu-item>
-            <el-menu-item index="3-2">选项2</el-menu-item>
+            <el-menu-item index="4-1">选项1</el-menu-item>
+            <el-menu-item index="4-2">选项2</el-menu-item>
           </el-menu-item-group>
           <el-menu-item-group title="分组2">
-            <el-menu-item index="3-3">选项3</el-menu-item>
+            <el-menu-item index="4-3">选项3</el-menu-item>
           </el-menu-item-group>
-          <el-submenu index="3-4">
+          <el-submenu index="4-4">
             <template slot="title">选项4</template>
-            <el-menu-item index="3-4-1">选项4-1</el-menu-item>
+            <el-menu-item index="4-4-1">选项4-1</el-menu-item>
           </el-submenu>
         </el-submenu>
       </el-menu>
@@ -107,8 +103,11 @@
           case '1-1':
             this.$router.push('/tests');
             break;
-          case '2':
-            this.$router.push('/forms');
+          case '2-1':
+            this.$router.push('/choiceManager');
+            break;
+          case '2-2':
+            this.$router.push('/subjectiveManager');
             break;
         }
       }
