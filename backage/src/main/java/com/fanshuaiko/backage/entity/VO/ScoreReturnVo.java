d47d9b0c563e70aad1529f98967473198976b617
fanshuaiko <1197303677@qq.com>
2019-04-18 10:18:27 +0800

    添加表格导出的注解

--- backage/src/main/java/com/fanshuaiko/backage/entity/VO/ScoreReturnVo.java
+++ backage/src/main/java/com/fanshuaiko/backage/entity/VO/ScoreReturnVo.java
@@ -1,5 +1,9 @@
 package com.fanshuaiko.backage.entity.VO;
 
+import cn.afterturn.easypoi.excel.annotation.Excel;
+
+import java.io.Serializable;
+
 /**
  * @ClassName ScoreReturnVo
  * @Description 学生分数返回实体类
@@ -7,12 +11,18 @@ package com.fanshuaiko.backage.entity.VO;
  * @Date 19-4-17 上午11:46
  * @Version 1.0
  **/
-public class ScoreReturnVo {
+public class ScoreReturnVo implements Serializable {
+    @Excel(name = "考试名称")
     private String testName; //考试名称
+    @Excel(name = "学号")
     private int studentNo; //学号
+    @Excel(name = "姓名")
     private String studentName; //姓名
+    @Excel(name = "专业")
     private String majorName; //专业
+    @Excel(name = "班级")
     private int classCo; //班级
+    @Excel(name = "总分")
     private int totalScore; //总分
 
     public String getTestName() {
