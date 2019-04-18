e3298b2b5cc666159c1c79b2ea0acd15cfd78a4e
fanshuaiko <1197303677@qq.com>
2019-04-18 20:08:46 +0800

    接收json格式的参数转换为对象

--- mail/src/main/java/com/fanshuaiko/mail/MailController.java
+++ mail/src/main/java/com/fanshuaiko/mail/MailController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.thymeleaf.TemplateEngine;
@@ -44,7 +45,7 @@ public class MailController {
      * @param scoreParams
      */
     @PostMapping("/score")
-    public String sendHTMLMail(ScoreParams scoreParams) {
+    public String sendHTMLMail(@RequestBody ScoreParams scoreParams) {
         Context context = new Context();
         //设置模板页面中的参数
         context.setVariable("name",scoreParams.getName());
