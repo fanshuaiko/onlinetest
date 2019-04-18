9d5a42c58db99534f644d1053062a9af4d05a2f5
fanshuaiko <1197303677@qq.com>
2019-04-18 20:07:26 +0800

    调用邮件服务失败时调用的方法

---
+++ backage/src/main/java/com/fanshuaiko/backage/service/impl/MailServiceFail.java
@@ -0,0 +1,22 @@
+package com.fanshuaiko.backage.service.impl;
+
+import com.fanshuaiko.backage.entity.mail.ScoreParams;
+import com.fanshuaiko.backage.service.MailService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName MailServiceFail
+ * @Description
+ * @Author fanshuaiko
+ * @Date 19-4-18 下午7:30
+ * @Version 1.0
+ **/
+@Component
+@RestController
+public class MailServiceFail implements MailService {
+    @Override
+    public String sendHTMLMail(ScoreParams scoreParams) {
+        return "调用失败，当前邮件服务不可用";
+    }
+}
