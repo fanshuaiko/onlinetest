ace6b4369aca1565551eef31264d2ed7049676cd
fanshuaiko <1197303677@qq.com>
2019-04-18 19:54:03 +0800

    添加feign注解

--- backage/src/main/java/com/fanshuaiko/backage/BackageApplication.java
+++ backage/src/main/java/com/fanshuaiko/backage/BackageApplication.java
@@ -4,9 +4,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 @MapperScan("com.fanshuaiko.backage.dao")
 public class BackageApplication {
 
